5ed6d82324db3ebb42891ada0f15545216d99beb
Viktoria Ovsyannikova <vichka.ovsyannikova07@gmail.com>
2024-01-09 07:52:15 +0300

    тестовый коммит

--- middlewares/celebrate.js
+++ middlewares/celebrate.js
@@ -27,7 +27,7 @@ const validationCreateMovie = celebrate({
   body: Joi.object().keys({
     country: Joi.string().required(),
     director: Joi.string().required(),
-    duration: Joi.number().required(),
+    duration: Joi.number().integer().required(),
     year: Joi.string().required(),
     description: Joi.string().required(),
     image: Joi.string().required().regex(regexUrl),
@@ -41,7 +41,7 @@ const validationCreateMovie = celebrate({
 
 const validationDeleteMovie = celebrate({
   params: Joi.object().keys({
-    id: Joi.string().hex().length(24),
+    movieId: Joi.string().required().hex().length(24),
   }),
 });
 
